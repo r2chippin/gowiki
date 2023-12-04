382c933681b1f3327dda72621e481a441f3de556
chippin <r2chippin@proton.me>
2023-12-04 21:44:31 +0800

    Finished edit func

--- controller/handler.go
+++ controller/handler.go
@@ -9,8 +9,28 @@ import (
 // ViewHandler sad
 func ViewHandler(w http.ResponseWriter, r *http.Request, td string) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := model.LoadPage(title, td)
-	_, err := fmt.Fprintf(w, "<h1>%s</h1>\n<div>%s</div>", p.Title, p.Body)
+	p, err := model.LoadPage(title, td)
+	if err != nil {
+		p = &model.Page{Title: title}
+	}
+	_, err = fmt.Fprintf(w, "<h1>%s</h1>\n<div>%s</div>", p.Title, p.Body)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func EditHandler(w http.ResponseWriter, r *http.Request, td string) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := model.LoadPage(title, td)
+	if err != nil {
+		p = &model.Page{Title: title}
+	}
+	_, err = fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
 	if err != nil {
 		panic(err)
 	}
--- wiki.go
+++ wiki.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"gowiki/controller"
-	model "gowiki/models"
 	"log"
 	"net/http"
 	"os"
@@ -21,31 +20,32 @@ type Config struct {
 var config Config
 
 func main() {
+	// Read config.json to get config info
 	configData, err := os.ReadFile("config.json")
 	if err != nil {
 		panic(err)
 	}
-
 	err = json.Unmarshal(configData, &config)
 	if err != nil {
 		panic(err)
 	}
+	// Show all info
 	fmt.Println("Server is starting...")
 	fmt.Println("Target Directory:", config.TargetDirectory)
 	fmt.Println("Log Level:", config.LogLevel)
 	fmt.Println("Max Retries:", config.MaxRetries)
 	fmt.Println("Timeout Seconds:", config.TimeoutSeconds)
 
-	p1 := &model.Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	err = p1.Save(config.TargetDirectory)
-	if err != nil {
-		panic(err)
-	}
-	p2, _ := model.LoadPage("TestPage", config.TargetDirectory)
-	fmt.Println(string(p2.Body))
-
 	http.HandleFunc("/view/", func(w http.ResponseWriter, r *http.Request) {
 		controller.ViewHandler(w, r, config.TargetDirectory)
 	})
+	http.HandleFunc("/edit/", func(w http.ResponseWriter, r *http.Request) {
+		controller.EditHandler(w, r, config.TargetDirectory)
+	})
+	/*
+		http.HandleFunc("/save/", func(w http.ResponseWriter, r *http.Request) {
+			controller.SaveHandler(w, r, config.TargetDirectory)
+		})
+	*/
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
